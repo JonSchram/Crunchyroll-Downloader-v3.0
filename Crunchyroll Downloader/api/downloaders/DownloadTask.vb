458a6299732dd3d1d7bf841e2d2d07b8cf18ff07
JonSchram <7951615+JonSchram@users.noreply.github.com>
2023-03-01 02:02:59 -0500

    Adds DownloadTask object.
    Encapsulates a video to be downloaded and allows it to be named correctly.
    Should make it easier to manage multiple downloads.

---
+++ Crunchyroll Downloader/api/downloaders/DownloadTask.vb
@@ -0,0 +1,7 @@
+﻿''' <summary>
+''' Class intended to represent a single download task.
+''' It is for a single video and contains all information needed to download it and name the file correctly.
+''' </summary>
+Public Class DownloadTask
+
+End Class
