9a557304bf7ff1856883f9b708cd62a3700c3bd3
JonSchram <7951615+JonSchram@users.noreply.github.com>
2023-10-28 14:02:14 -0400

    Adds parser for ffmpeg output.
    This will be instantiated by the ffmpeg adapter to check how much progress has been made on the file, but I'm not quite sure how it will be wired up yet.

---
+++ CrunchyrollDownloaderTests/utilities/FfmpegOutputParserTest.vb
@@ -0,0 +1,18 @@
+﻿Imports Crunchyroll_Downloader.utilities
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+Namespace utilities
+    <TestClass>
+    Public Class FfmpegOutputParserTest
+        <TestMethod>
+        Public Sub TestHandleFfmepgOutput_Duration()
+            Dim outputLine = "  Duration: 00:22:28.12, start: 0.000000, bitrate: 9001 kb/s"
+
+            Dim outputParser As New FfmpegOutputParser()
+            outputParser.HandleFfmpegOutput(outputLine)
+
+            Dim expectedDuration As Double = 22 + 28.12 / 60
+            Assert.AreEqual(expectedDuration, outputParser.GetDuration(), 0.01)
+        End Sub
+    End Class
+End Namespace
